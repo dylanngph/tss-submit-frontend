db5dc72b8dd9a124efc3e8d7616c88330bda665f
Quang Huy <quanhuyc12ltqtm@gmail.com>
2022-02-16 11:35:52 +0700

    fix code

--- src/sagas/account/postLoginAccount.saga.js
+++ src/sagas/account/postLoginAccount.saga.js
@@ -6,37 +6,37 @@ import { constants as loadingConstant } from "redux/loading";
 import Notify from "components/custom/Notify";
 
 function* doAction(action) {
-  try {
-    yield put({ type: loadingConstant.SHOW_LOADING });
-    const resData = yield call(accountApi.postLoginAccount, action.payload);
-    const { code, data, message } = resData;
-    switch (code) {
-      case REQUEST_STATUS.SUCCESS:
-        yield put({ type: loadingConstant.HIDE_LOADING });
-        yield put({
-          type: constants.ACCOUNT_LOGIN_SUCCESS,
-          payload: { code, data, message },
-        });
-        break;
-      default:
-        yield put({ type: loadingConstant.HIDE_LOADING });
-        Notify({content: message || 'Hệ thống xảy ra lỗi'})
-        yield put({
-          type: constants.ACCOUNT_LOGIN_FAIL,
-          payload: { data, message },
-        });
-        if (action.callback) {
-          action.callback(false, data, code, message);
-        }
-    }
-  } catch (error) {
-    yield put({ type: loadingConstant.HIDE_LOADING });
-    Notify({content: error || 'Hệ thống xảy ra lỗi'})
-    yield put({ type: constants.ACCOUNT_LOGIN_FAIL });
-    if (action.callback) {
-      action.callback(false, error);
-    }
-  }
+  // try {
+  //   yield put({ type: loadingConstant.SHOW_LOADING });
+  //   const resData = yield call(accountApi.postLoginAccount, action.payload);
+  //   const { code, data, message } = resData;
+  //   switch (code) {
+  //     case REQUEST_STATUS.SUCCESS:
+  //       yield put({ type: loadingConstant.HIDE_LOADING });
+  //       yield put({
+  //         type: constants.ACCOUNT_LOGIN_SUCCESS,
+  //         payload: { code, data, message },
+  //       });
+  //       break;
+  //     default:
+  //       yield put({ type: loadingConstant.HIDE_LOADING });
+  //       Notify({content: message || 'Hệ thống xảy ra lỗi'})
+  //       yield put({
+  //         type: constants.ACCOUNT_LOGIN_FAIL,
+  //         payload: { data, message },
+  //       });
+  //       if (action.callback) {
+  //         action.callback(false, data, code, message);
+  //       }
+  //   }
+  // } catch (error) {
+  //   yield put({ type: loadingConstant.HIDE_LOADING });
+  //   Notify({content: error || 'Hệ thống xảy ra lỗi'})
+  //   yield put({ type: constants.ACCOUNT_LOGIN_FAIL });
+  //   if (action.callback) {
+  //     action.callback(false, error);
+  //   }
+  // }
 }
 
 export default function* postLoginAccount() {
